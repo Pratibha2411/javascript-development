7f061770f84e8f407b56af313dcc05e0cb0ef750
Pratibha2411 <pbaloriya@gmail.com>
2023-01-24 22:59:44 +0530

    added

--- arrays.js
+++ arrays.js
@@ -1,1 +1,19 @@
 // array = think of it as a variable that can store multiple values
+
+let fruits = ["apple", "orange", "banana"];
+
+// fruits[2] = "coconut"; //add element on given index and replce present one if there is any
+
+fruits.push("Lemon"); //add an element at the end
+// fruits.pop();  //removes last element
+fruits.unshift("Mango"); //add an element at beginning
+// fruits.shift();  //removes element from beginning
+
+let lengthOfFruits = fruits.length; //shows no of element in an array
+let indexOfFruits = fruits.indexOf("apple");
+// let indexOfFruit = fruits.indexOf("Kiwi");
+
+console.log(lengthOfFruits);
+console.log(indexOfFruits);
+// console.log(indexOfFruit);
+console.log(fruits);
---
+++ for-of_statement.js
@@ -0,0 +1,3 @@
+// iterate the elements to an array using standard for loop:
+
+
