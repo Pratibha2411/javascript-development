71adfaeaed90e80ad83a35b81a6b6b844781c888
Pratibha2411 <pbaloriya@gmail.com>
2023-01-29 17:04:11 +0530

    added

--- inheritance.js
+++ inheritance.js
@@ -1,1 +1,18 @@
-// inheritance = inheritance is when a child class inherits all the methods and properties from another class.
+// inheritance = inheritance is when a child class inherits all the methods and properties from another class. '
+
+class Rabbit{
+    alive = true;
+    name = "rabbit";
+
+    eat(){
+        console.log(`This ${this.name} is eating`);
+    }
+    sleep(){
+        console.log(`This ${this.name} is sleeping`);
+    }
+    run(){
+        console.log(`This ${this.name} is running`);
+    }
+    
+
+}
