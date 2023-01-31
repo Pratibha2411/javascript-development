604e9d21557e95dfe7789797212a1d5a5023736a
Pratibha2411 <pbaloriya@gmail.com>
2023-01-31 21:52:29 +0530

    added

--- inheritance.js
+++ inheritance.js
@@ -1,8 +1,58 @@
 // inheritance = inheritance is when a child class inherits all the methods and properties from another class. '
 
-class Rabbit{
+// 1st method: this is wrong coz we're repeating so many lines of code and in programming we dont repeat code = 
+
+// class Rabbit{
+//     alive = true;
+//     name = "rabbit";
+
+//     eat(){
+//         console.log(`This ${this.name} is eating`);
+//     }
+//     sleep(){
+//         console.log(`This ${this.name} is sleeping`);
+//     }
+//     run(){
+//         console.log(`This ${this.name} is running`);
+//     }
+// }
+
+// class Fish{
+//     alive = true;
+//     name = "fish";
+
+//     eat(){
+//         console.log(`This ${this.name} is eating`);
+//     }
+//     sleep(){
+//         console.log(`This ${this.name} is sleeping`);
+//     }
+//     swim(){
+//         console.log(`This ${this.name} is swimming`);
+//     }
+// }
+// class Hawk{
+//     alive = true;
+//     name = "hawk";
+
+//     eat(){
+//         console.log(`This ${this.name} is eating`);
+//     }
+//     sleep(){
+//         console.log(`This ${this.name} is sleeping`);
+//     }
+//     fly(){
+//         console.log(`This ${this.name} is flying`);
+//     }
+// }
+// const rabbit = new Rabbit();
+// const fish = new Fish();
+// const hawk = new Hawk();
+
+
+// 2nd method: productive and right way to do things =
+class Animal{
     alive = true;
-    name = "rabbit";
 
     eat(){
         console.log(`This ${this.name} is eating`);
@@ -10,9 +60,46 @@ class Rabbit{
     sleep(){
         console.log(`This ${this.name} is sleeping`);
     }
+
+}
+
+class Rabbit extends Animal{
+    name = "rabbit";
+
     run(){
         console.log(`This ${this.name} is running`);
     }
-    
-
 }
+
+class Fish extends Animal{
+    name = "fish";
+
+    swim(){
+        console.log(`This ${this.name} is swimming`);
+    }
+}
+class Hawk extends Animal{
+    name = "hawk";
+
+    fly(){
+        console.log(`This ${this.name} is flying`);
+    }
+}
+const rabbit = new Rabbit();
+const fish = new Fish();
+const hawk = new Hawk();
+
+console.log(rabbit.alive);
+rabbit.eat();
+rabbit.sleep();
+rabbit.run();
+
+console.log(fish.alive);
+fish.eat();
+fish.sleep();
+fish.swim();
+
+console.log(hawk.alive);
+hawk.eat();
+hawk.sleep();
+hawk.fly();
